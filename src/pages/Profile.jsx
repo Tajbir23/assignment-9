17cb540adf421650918bea5f8d22435755567352
Tajbir islam <studenttajbirislam@gmail.com>
2024-04-15 22:28:49 +0600

    fix profile responsive

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -43,22 +43,22 @@ const Profile = () => {
     </Helmet>
     <div className="h-screen flex items-center justify-center">
         {loading ? <h1>Loading</h1> : <>
-        <div className="card lg:card-side bg-base-100 shadow-xl">
-  <figure><img src={user?.photo} alt="Album"/></figure>
-  <div className="card-body">
-    <div>
-        <h1 className='font-bold text-4xl'>{user.name}</h1>
-        <h3 className='font-semibold text-3xl'>{user.email}</h3>
-    </div>
-    <div className="card-actions justify-end">
-      <Link to={`/updateProfile/${id}`} className="btn btn-primary">Update profile</Link>
-    </div>
-  </div>
+        <div className="max-w-xs rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
+	<img src={user?.photo} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-3xl font-semibold tracking-wide">{user?.name}</h2>
+			<p className="dark:text-gray-800">{user?.email}</p>
+		</div>
+		<Link to={`/updateProfile/${id}`} className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-600 dark:text-gray-50">Update profile</Link>
+	</div>
 </div>
         </>}
     </div>
+
     </>
   )
 }
+
 
 export default Profile
